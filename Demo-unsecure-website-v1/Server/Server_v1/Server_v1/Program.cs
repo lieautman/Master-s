3917bc68d89ecfc53fc92fbebe6e9ede75bb0411
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-02-18 14:28:33 +0200

    added server to v1

---
+++ Demo-unsecure-website-v1/Server/Server_v1/Server_v1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+const string DatabaseConnString = "Data Source=../../../../../../Database/db_v1.db";
+
+using (var connection = new SqliteConnection(DatabaseConnString))
+{
+    Console.WriteLine("The accounts in database are");
+
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT *
+        FROM Accounts
+    ";
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var id = reader.GetString(0);
+            var username = reader.GetString(1);
+            var password = reader.GetString(2);
+
+            Console.WriteLine($"{id} {username} {password}");
+        }
+    }
+}
