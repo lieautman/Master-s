4e8b6f2ba5adf9aeb218b2c144f19804abacc50b
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-03-03 11:43:16 +0200

    rename folders and made server work

---
+++ PracticalProject/server/server/Controllers/AccountsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server_v1.ReadObjects;
+using Microsoft.Data.Sqlite;
+
+namespace Server_v1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        const string DatabaseConnString = "Data Source=../../database/db_v1.db";
+
+        [HttpGet(Name = "GetAccounts")]
+        public IEnumerable<Account> Get()
+        {
+            using (var connection = new SqliteConnection(DatabaseConnString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM Accounts";
+
+                List<Account> list = new List<Account>();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = Int32.Parse(reader.GetString(0));
+                        var username = reader.GetString(1);
+                        var password = reader.GetString(2);
+
+                        Account a = new Account(id, username, password);
+                        list.Add(a);
+                    }
+                }
+                return list;
+            }
+        }
+
+        [HttpPost(Name = "SignIn")]
+        public Boolean Post(Account a)
+        {
+            using (var connection = new SqliteConnection(DatabaseConnString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT Password FROM Accounts where Username = '" + a.username + "' and Password = '" + a.password + "'";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        //destructive payload
+        /*        {
+          "id": 0,
+          "username": "a",
+          "password": "a' or 1=1--"
+        }*/
+
+    }
+}
