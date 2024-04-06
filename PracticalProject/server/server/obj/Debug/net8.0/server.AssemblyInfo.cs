509a9fa3c4c9c0c14940c2610f3a957a42026890
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-04-06 17:58:24 +0300

    push modif

--- PracticalProject/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ PracticalProject/server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aeea6349e2bbe0e1708a22990ac357393f9358b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e833053bf18000ec93ba6ab31b8fb39ad0a111")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
