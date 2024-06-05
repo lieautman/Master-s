20816229bc7c61cc6a7f7f1d50d51da14822dffc
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-06-05 16:32:33 +0300

    fix ssrf

--- PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/InsecureRequest/index.js
+++ PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/InsecureRequest/index.js
@@ -7,7 +7,11 @@ function InsecureRequest() {
 
     return (
         <>
-            <p>{`To fix the problem, a check on the imput can be used.`}</p>
+            <p>{`This problem may occur when the api is too permisive with the imputs.`}</p>
+            <p>{`For example if the API reads a text file, if the API receives the file name,
+            an attacker can modify it and get access to other sensitive data.`}</p>
+            <p>{`The input below takes in resource name (this kind of input may not exist but can be replicated with programs such as Postman).
+             The text1 resource is intended to be presented, but text2 is not.`}</p>
             <input type="text" defaultValue={"text1"} onChange={(event) => setFileName(event.target.value)} />
             <input type="button" value="Get resource" onClick={() => { ServerSideRequestForgeryLevel1(setData, fileName) }} />
 
--- PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/SecureRequest/index.js
+++ PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/SecureRequest/index.js
@@ -7,11 +7,7 @@ function SecureRequest() {
 
     return (
         <>
-            <p>{`This problem may occur when the api is too permisive with the imputs.`}</p>
-            <p>{`For example if the API reads a text file, if the API receives the file name,
-            an attacker can modify it and get access to other sensitive data.`}</p>
-            <p>{`The input below takes in resource name (this kind of input may not exist but can be replicated with programs such as Postman).
-             The text1 resource is intended to be presented, but text2 is not.`}</p>
+             <p>{`To fix the problem, a check on the imput can be used.`}</p>
             <input type="text" defaultValue={"text1"} onChange={(event) => setFileName(event.target.value)} />
             <input type="button" value="Get resource" onClick={() => { ServerSideRequestForgeryLevel2(setData, fileName) }} />
 
