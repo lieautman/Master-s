cc49ec52f076922575a1430dc7d29c51f9f04872
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-06-06 23:45:09 +0300

    css and text

--- PracticalProject/client/src/PossibleSubjects/index.js
+++ PracticalProject/client/src/PossibleSubjects/index.js
@@ -8,7 +8,7 @@ function PossibleSubjects({ setListOfTopics }) {
     return (
         <>
             <Typography textAlign={"center"} color={'white'}>Select a language to receive security issues related to it:</Typography>
-            <Grid container spacing={2} width={"50vw"} >{listOfLanguagesWithTags.map(languageWithTag =>
+            <Grid container spacing={2} width={"70vw"} >{listOfLanguagesWithTags.map(languageWithTag =>
                 <Grid item xs={3} textAlign={"center"} style={{ padding: "50px" }} key={languageWithTag.id}>
                     <Tooltip title={languageWithTag.tags.length !== 0 ? "Tags: " + languageWithTag.tags.map(element => element.name
                     ).join(", ") : ""}><Button variant="contained" size={"large"} onClick={() => getListOfTopics(setListOfTopics, languageWithTag.tags)}>{languageWithTag.name} </Button>
