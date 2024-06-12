fa6f5c5d23b633afbf4925e9ddc6178392d264ef
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-06-12 03:11:29 +0300

    add postman to object property level authorisation

--- PracticalProject/client/src/TypesOfAttacks/ObjectPropertyLevelAuthorisationAttack/index.js
+++ PracticalProject/client/src/TypesOfAttacks/ObjectPropertyLevelAuthorisationAttack/index.js
@@ -2,6 +2,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { useState } from 'react';
 import InsecureDataLoading from './InsecureDataLoading';
 import SecureDataLoading from './SecureDataLoading';
+import { Button } from '@mui/material';
 
 function ObjectPropertyLevelAuthorisationAttack() {
   const [tabIndex, setTabIndex] = useState(0);
@@ -10,6 +11,9 @@ function ObjectPropertyLevelAuthorisationAttack() {
     <>
       <p>{"Threat description:"}</p>
       <p>{`This attack is based on if the attacker has access to more data then intended.`}</p>
+      <p>{`Postmand website link: `} <a href='https://www.postman.com/' target="_blank" rel="noreferrer" >{`Postman website`}</a></p>
+      <p>{`Postman import file: `} <Button variant="contained" href="object_property_level_authorisation_postman.json" download="object_property_level_authorisation_postman.json"> {` Download postman file`} </Button></p>
+
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
           <Tab>Lv.1 Insecure data loading</Tab>
