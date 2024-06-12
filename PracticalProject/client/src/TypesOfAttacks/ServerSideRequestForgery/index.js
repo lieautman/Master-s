e994d4bc6e008569a3be19195e3ce6eb5c5b9f5a
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-06-12 03:16:23 +0300

    add postman to server side request forgery

--- PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/index.js
+++ PracticalProject/client/src/TypesOfAttacks/ServerSideRequestForgery/index.js
@@ -2,6 +2,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { useState } from 'react';
 import InsecureRequest from "./InsecureRequest"
 import SecureRequest from "./SecureRequest"
+import { Button } from '@mui/material';
 
 
 function ServerSideRequestForgery() {
@@ -12,6 +13,9 @@ function ServerSideRequestForgery() {
       <p>{"Threat description:"}</p>
       <p>{`The Server side request forgery allows an attacker to make requests as the API server to resources.
        This can be dangerous becaause this may lead to the site malfunctioning.`}</p>
+      <p>{`Postmand website link: `} <a href='https://www.postman.com/' target="_blank" rel="noreferrer" >{`Postman website`}</a></p>
+      <p>{`Postman import file: `} <Button variant="contained" href="server_side_request_forgery.json" download="server_side_request_forgery.json"> {` Download postman file`} </Button></p>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}></Tabs>
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
           <Tab>Lv.1 Insecure Request</Tab>
